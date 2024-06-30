5d8659cf50cd46ee237a94d35c113940bd9dd679
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-01 02:51:00 +0800

    Added index/home page and about page, imported and used fs, url, and path modules in routing.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer( async (req, res) => {
 
     // Sample codes to input or output in server
     // res.write('Hello World!');
@@ -24,20 +27,44 @@ const server = http.createServer((req, res) => {
     // console.log(req.url);
     // console.log(req.method);
 
+    // Get current path ( if using Common JS Modules )
+    // __filename
+    // __dirname
+
+    // Get current path ( for ES Modules )
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    console.log(`File name: ${__filename}`);
+    console.log(`Directory name: ${__dirname}`);
+
     try {
         // Check if GET request
         if(req.method == 'GET') {
+            let filePath;
+
             // Sample Code to create a router
             if (req.url === '/') {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end('<h1>Homepage</h1>');
+                // res.writeHead(200, { 'Content-Type': 'text/html' });
+                // res.end('<h1>Homepage</h1>');
+                //  use this instead for fs module...
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if(req.url === '/about') {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end('<h1>About</h1>');
+                // res.writeHead(200, { 'Content-Type': 'text/html' });
+                // res.end('<h1>About</h1>');
+                //  use this instead for fs module...
+                filePath = path.join(__dirname, 'public', 'about.html');
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.end('<h1>Page not found</h1>');
+                // res.writeHead(404, { 'Content-Type': 'text/html' });
+                // res.end('<h1>Page not found</h1>');
+                //  use this instead for fs module...
+                throw new Error('Not Found');
             }
+
+            const data = await fs .readFile(filePath);
+            res.setHeader('Content-type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error('Method not allow');
         }
