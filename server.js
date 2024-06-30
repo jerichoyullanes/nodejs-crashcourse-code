cef3f318901194aac1b8092b35d1d4f77f6048a2
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-01 00:51:15 +0800

    Added HTTP Module and Create Server

---
+++ server.js
@@ -0,0 +1,23 @@
+import http from 'http';
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+
+    // sample code
+    // res.write('Hello World!');
+    // res.end();
+    // or
+    // res.end('Hello World!');
+
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+    // res.end('<h1> Hello World! </h1>');
+    // or
+    
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({message: 'Server Error'}));
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
