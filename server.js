35c1219e84650febb400a48374ac7f49c36024a0
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-01 01:16:33 +0800

    Change some code in server.js

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ const server = http.createServer((req, res) => {
     // res.end('<h1> Hello World! </h1>');
     // or
     
-    res.writeHead(500, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({message: 'Server Error'}));
+    // res.writeHead(500, { 'Content-Type': 'application/json' })
+    // res.end(JSON.stringify({message: 'Server Error'}));
+
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end('<h1> Hello World! </h1>');
 });
 
 server.listen(PORT, () => {
