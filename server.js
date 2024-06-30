af60d4c41a4a18db8361c262db1f47c0f345d6c0
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-01 01:33:17 +0800

    Added env file and declare the port in it, also put some console log for requests in js server file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
 
@@ -16,6 +16,9 @@ const server = http.createServer((req, res) => {
     
     // res.writeHead(500, { 'Content-Type': 'application/json' })
     // res.end(JSON.stringify({message: 'Server Error'}));
+
+    console.log(req.url);
+    console.log(req.method);
 
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end('<h1> Hello World! </h1>');
