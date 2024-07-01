2d4fb8894ea47aeef38b1771a8e94cf8b4bc3425
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-02 06:18:43 +0800

    Added OS module functions samples

---
+++ osDemo.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+// userInfo - return an object of User Information
+console.log(os.userInfo());
+
+// totalmem - return total memory
+console.log(os.totalmem());
+
+// freemem - total free memory
+console.log(os.freemem()); 
+
+// cpus() - return array object of CPU information  
+console.log(cpus());
